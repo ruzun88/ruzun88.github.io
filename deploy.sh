0a88698a4b832fd59146d84574cbab0e00f09f16
ruzun88 <ruzun88@gmail.com>
2021-01-29 23:43:24 +0900

    workflow 원복

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,23 @@
-#!/usr/bin/env sh
+# #!/usr/bin/env sh
 
-# abort on errors
-set -e
+# # abort on errors
+# set -e
 
-# build
-npm run docs:build
+# # build
+# npm run docs:build
 
-# navigate into the build output directory
-cd docs/.vuepress/dist
+# # navigate into the build output directory
+# cd docs/.vuepress/dist
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+# # if you are deploying to a custom domain
+# # echo 'www.example.com' > CNAME
 
-git init
-git add -A
-git commit -m 'deploy'
+# git init
+# git add -A
+# git commit -m 'deploy'
 
 
-# if you are deploying to https://<USERNAME>.github.io
-git push -f https://github.com/ruzun88/ruzun88.github.io.git master
+# # if you are deploying to https://<USERNAME>.github.io
+# # git push -f https://github.com/ruzun88/ruzun88.github.io.git master
 
-cd -
+# cd -
